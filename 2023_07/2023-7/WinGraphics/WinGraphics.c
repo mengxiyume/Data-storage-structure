e6a5e8c51ed6f2e7d1ec523542f2cf486f1c5d93
mengxiyume <94777252+mengxiyume@users.noreply.github.com>
2023-07-19 22:46:56 +0800

    2023-07-19-22-46
    Windows API

--- 2023_07/2023-7/WinGraphics/WinGraphics.c
+++ 2023_07/2023-7/WinGraphics/WinGraphics.c
@@ -1,12 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include<Windows.h>
+#include<assert.h>
 
-void PaintLine(HWND hWnd)
+void PaintLine(HDC hdc)
 {
-	PAINTSTRUCT ps = { 0 };
-	HDC hdc = BeginPaint(hWnd, &ps);
-
 	POINT arrPtPolyPolyLine[] = {
 		110,60, 10,60, 60,10, 160,10,
 		10,60, 10,160, 110,160, 110,60, 160,10, 160,110, 110,160,
@@ -17,9 +15,6 @@ void PaintLine(HWND hWnd)
 	POINT arrPtPolyLineTo[] = { 110,260, 210,240 };
 	LOGPEN logPen = { 0 };
 	HPEN hPen = NULL;
-
-
-	SetBkMode(hdc, TRANSPARENT);
 
 	logPen.lopnColor = 0x9CC99C;
 	logPen.lopnStyle = PS_SOLID;
@@ -60,16 +55,10 @@ void PaintLine(HWND hWnd)
 	MoveToEx(hdc, 10, 240, NULL);
 	PolylineTo(hdc, arrPtPolyLineTo, _countof(arrPtPolyLineTo));
 	DeleteObject(SelectObject(hdc, hPen));
-
-
-
-	EndPaint(hWnd, &ps);
 }
 
-void PaintArc(HWND hWnd)
+void PaintArc(HDC hdc)
 {
-	PAINTSTRUCT ps = { 0 };
-	HDC hdc = BeginPaint(hWnd, &ps);
 	HPEN hPen = CreatePen(PS_SOLID, 5, 0x000000);
 
 	hPen = SelectObject(hdc, hPen);
@@ -94,19 +83,14 @@ void PaintArc(HWND hWnd)
 	//Arc(hdc, 10, 10, 110, 110, 15, 15, 50, 150);
 
 	DeleteObject(SelectObject(hdc, hPen));
-	EndPaint(hWnd, &ps);
 }
 
-void PaintBezier(HWND hWnd)
+void PaintBezier(HDC hdc)
 {
 	POINT position = { 100,100 };
-	PAINTSTRUCT ps = { 0 };
 	POINT arrPoint[] = { position.x,position.y, position.x,position.x + 200, position.x + 300,position.y, position.x + 300,position.y + 200 };
-	HDC hdc = BeginPaint(hWnd, &ps);
 	HPEN hPen_1 = CreatePen(PS_DASH, 1, 0x56BB56);
 	HPEN hPen_2 = CreatePen(PS_SOLID, 2, 0x000000);
-
-	SetBkMode(hWnd, TRANSPARENT);
 
 	hPen_2 = SelectObject(hdc, hPen_2);
 	PolyBezier(hdc, arrPoint, _countof(arrPoint));
@@ -121,31 +105,124 @@ void PaintBezier(HWND hWnd)
 
 	DeleteObject(hPen_1);
 	DeleteObject(hPen_2);
-
-	EndPaint(hWnd, &ps);
 }
 
-void PaintPolyDraw(HWND hWnd)
+void PaintPolyDraw(HDC hdc)
 {
 	POINT arrPoint[] = { 10,100, 100,10, 150,150, 200,50, 10,100, 100,10 , 150,150, 200,50 };
 	BYTE arrFlag[] = { PT_MOVETO, PT_BEZIERTO, PT_BEZIERTO, PT_BEZIERTO, PT_MOVETO, PT_LINETO, PT_MOVETO, PT_LINETO };
-	PAINTSTRUCT ps = { 0 };
-	HDC hdc = BeginPaint(hWnd, &ps);
 	HPEN hPen = CreatePen(PS_SOLID, 2, 0x000000);
 	SelectObject(hdc, hPen);
 
 	PolyDraw(hdc, arrPoint, arrFlag, _countof(arrPoint));
 
 	DeleteObject(SelectObject(hdc, hPen));
-	EndPaint(hWnd, &ps);
+}
+
+void PaintBox(HDC hdc)
+{
+	POINT position = { 50,50 };
+	POINT arrGonPoint[] = { 500,500, 100,500, 100,100, 400,100, 400,400 , 400,200, 200,200, 200,400  };
+	
+	Rectangle(hdc, position.x, position.y, position.x + 200, position.y + 100);
+	position.x += 250;
+
+	RoundRect(hdc, position.x, position.y, position.x + 200, position.y + 100, 50, 50);
+	position.x += 250;
+
+	Ellipse(hdc, position.x, position.y, position.x + 200, position.y + 100);
+	position.x += 250;
+
+	SetArcDirection(hdc, AD_CLOCKWISE);
+	Chord(hdc, position.x, position.y, position.x + 200, position.y + 100, position.x + 200 * 3 / 4, position.y, position.x, position.y + 100 * 3 / 4);
+	position.x += 250;
+
+	Pie(hdc, position.x, position.y, position.x + 200, position.y + 100, position.x + 200 * 3 / 4, position.y, position.x, position.y + 100 * 3 / 4);
+	position.x += 250;
+
+	SetArcDirection(hdc, AD_COUNTERCLOCKWISE);
+	Chord(hdc, position.x, position.y, position.x + 200, position.y + 100, position.x + 200 * 3 / 4, position.y, position.x, position.y + 100 * 3 / 4);
+	position.x += 150;
+
+	Pie(hdc, position.x, position.y, position.x + 200, position.y + 100, position.x + 200 * 3 / 4, position.y, position.x, position.y + 100 * 3 / 4);
+	position.x = 50, position.y += 150;
+
+	Polygon(hdc, arrGonPoint, _countof(arrGonPoint));
+}
+
+void PaintStar(HDC hdc)
+{
+	POINT arrStarPoint_1[] = { 0,40, 100,40, 20,100, 50,0, 80,100 };
+	POINT arrStarPoint_2[] = { 120,40, 220,40, 140,100, 170,0, 200,100 };
+
+	SetPolyFillMode(hdc, ALTERNATE);
+	Polygon(hdc, arrStarPoint_1, _countof(arrStarPoint_1));
+	SetPolyFillMode(hdc, WINDING);
+	Polygon(hdc, arrStarPoint_2, _countof(arrStarPoint_2));
+
+}
+
+void PaintSandwitch(HDC hdc)
+{
+	POINT arrSandwitchPoint[] = 
+	{	50,66, 66,33, 50,0, 33,33,
+		50,66, 17,66, 0,100, 33,100,
+		50,66, 83,66, 100,100, 66,100 };
+
+	INT arrPolyCounts[] = { 4, 4, 4 };
+
+	PolyPolygon(hdc, arrSandwitchPoint, arrPolyCounts, _countof(arrPolyCounts));
+}
+
+void Graphics_PosTrans(HDC hdc)
+{
+	XFORM xForm = { 0 };
+	HPEN hPen = CreatePen(PS_SOLID, 2, 0x000000);
+	HBRUSH hBrush = CreateSolidBrush(0xFFFFFF);
+
+	SetMapMode(hdc, MM_LOENGLISH);
+	hPen = SelectObject(hdc, hPen);
+	hBrush = SelectObject(hdc, hBrush);
+	
+	Rectangle(hdc, 50,-50,650,-650);
+
+	DeleteObject(SelectObject(hdc, hPen));
+	DeleteObject(SelectObject(hdc, hBrush));
+	SetMapMode(hdc, MM_TEXT);
 }
 
 void MessageProc_WM_PAINT(HWND hWnd)
 {
-	//PaintLine(hWnd);
-	//PaintArc(hWnd);
-	//PaintBezier(hWnd);
-	PaintPolyDraw(hWnd);
+	PAINTSTRUCT ps = { 0 };
+	HDC hdc = BeginPaint(hWnd, &ps);
+	RECT clientRect = { 0 };
+	SetBkMode(hWnd, TRANSPARENT);
+
+	//PaintLine(hdc);
+	//PaintArc(hdc);
+	//PaintBezier(hdc);
+	//PaintPolyDraw(hdc);
+	//PaintBox(hdc);
+	//PaintStar(hdc);
+	//PaintSandwitch(hdc);12
+
+	//Graphics_PosTrans(hdc);
+	//SetGraphicsMode(hWnd, GM_ADVANCED);
+
+	//SetMapMode(hdc, MM_LOENGLISH);
+	//DPtoLP(hdc, (LPPOINT)&clientRect, 2);
+	//Ellipse(hdc, clientRect.right / 4, clientRect.bottom / 4, clientRect.right * 3 / 4, clientRect.bottom * 3 / 4);
+	SetMapMode(hdc, MM_ISOTROPIC);
+	SetWindowExtEx(hdc, 500, 500, NULL);
+	GetClientRect(hWnd, &clientRect);
+	SetViewportExtEx(hdc, clientRect.right, -clientRect.bottom, NULL);
+
+	SetViewportOrgEx(hdc, clientRect.right / 2, clientRect.bottom / 2, NULL);
+
+	Ellipse(hdc, -100, 100, 100, -100);
+
+	SetMapMode(hdc, MM_TEXT);
+	EndPaint(hdc, &ps);
 }
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msgID, WPARAM wParam, LPARAM lParam)
@@ -176,7 +253,7 @@ int CALLBACK WinMain(HINSTANCE hIns, HINSTANCE hPrevIns, LPSTR lpCmdLine, int nC
 	wcEx.cbClsExtra = 0;
 	wcEx.cbSize = sizeof(WNDCLASSEX);
 	wcEx.cbWndExtra = 0;
-	wcEx.hbrBackground = CreateSolidBrush(0xFFFFFF);
+	wcEx.hbrBackground = CreateSolidBrush(0xAAAAAA);
 	wcEx.hCursor = NULL;
 	wcEx.hIcon = LoadIcon(NULL, IDI_SHIELD);
 	wcEx.hIconSm = LoadIcon(NULL, IDI_INFORMATION);
